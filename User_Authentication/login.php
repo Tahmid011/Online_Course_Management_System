cd25e00344aae611b312a12ea6b805f27c923dc1
Tahmid <biswastahmidul@gmail.com>
2025-08-25 23:48:26 +0600

    add repo

---
+++ User_Authentication/login.php
@@ -0,0 +1,22 @@
+<!DOCTUPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Page</title>
+        <link rel="stylesheet" href="../css/login.css">
+    </head>
+    <body>
+        <form class="login" method="POST" >
+            <h1>Online Course Registration</h1>
+            <div id="container">
+                <input type="email" id="email" name="email" placeholder="Enter your Email" required>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+
+                <button type="submit" id="login-btn" name="login-btn">Login</button>
+                
+
+
+            </div>
+        </form>
+    </body>
+</html>
