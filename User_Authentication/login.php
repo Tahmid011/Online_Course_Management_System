86370ce8a3aac44be1204abada3635c2c5620a07
Tahmid <biswastahmidul@gmail.com>
2025-08-28 00:47:07 +0600

    add register

--- User_Authentication/login.php
+++ User_Authentication/login.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Login Page</title>
-        <link rel="stylesheet" href="../css/login.css">
+        <link rel="stylesheet" href="/Online_Course_Management_System/User_Authentication/css/login.css">
     </head>
     <body>
         <form class="login" method="POST" >
@@ -13,9 +13,10 @@
                 <input type="password" id="password" name="password" placeholder="Enter your password" required>
 
                 <button type="submit" id="login-btn" name="login-btn">Login</button>
-                
-
-
+            </div>
+            <div class="signup">
+            <p>Don't have an account </p>
+            <a href="register.php" class="signup-button">Sign Up</a>
             </div>
         </form>
     </body>
---
+++ User_Authentication/register.php
@@ -0,0 +1,36 @@
+<!DOCTUPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Page</title>
+        <link rel="stylesheet" href="/Online_Course_Management_System/User_Authentication/css/register.css">
+    </head>
+    <body>
+        <form class="register" method="POST">
+            <h1>Online Course Registration</h1>
+            <div id="container">
+                <select name="role" required>
+                    <option>Select your role</option>
+                    <option>Admin</option>
+                    <option>Student</option>
+                </select>
+                <input type="text" name="name" placeholder="Enter your full name" required>
+                <input type="text" name="studentId" pattern="[0-9]{2}-[0-9]{5}-[0-9]{1}" placeholder="Enter your ID" required>
+                <select name="department" required>
+                    <option>Select your department</option>
+                    <option>CSE</option>
+                    <option>BBA</option>
+                    <option>IPE</option>
+                    <option>LLB</option>
+                </select>
+                <input type="tel" name="phone-no" pattern="01[0-9]{9}" placeholder="Enter your phone no" required>
+                <input type="email" name="email" placeholder="Enter your Email" required>
+                <input type="password" name="password" placeholder="Enter your password" required>
+                <input type="password" name="confirm-password" placeholder="Confirm your password " required>
+                <div class="signup">
+                    <button type="submit" name="signup-btn">Sign UP</button>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
